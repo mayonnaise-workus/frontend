f69cfd4571bda210c6594c7e09d3d2eecbce4a59
imssein <hyuntpdls@gmail.com>
2023-02-24 22:39:21 +0900

    feat: 워크 스페이스 선호 설정 화면 구현

---
+++ src/screens/RegisterWorkSpaceScreen/style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const FilterList = styled.View`
+  position: absolute;
+  left: 20px;
+  top: 196px;
+`;
+
+export const ButtonView = styled.View`
+  position: absolute;
+  width: 350px;
+  height: 60px;
+  top: 750px;
+`;
