92f0b82e8676513fa4fca2d5814c25f5b2451515
imssein <hyuntpdls@gmail.com>
2023-02-24 22:41:02 +0900

    feat: 회원가입시 사용하는 데이터

---
+++ src/data.js
@@ -0,0 +1,67 @@
+export const region = [
+  {id: 1, title: '강남역', enTitle: 'gannam'},
+  {id: 2, title: '선릉역', enTitle: 'seolleung'},
+  {id: 3, title: '삼성역', enTitle: 'samsung'},
+  {id: 4, title: '역삼역', enTitle: 'yeoksam'},
+  {id: 5, title: '논현역', enTitle: 'nonhyeon'},
+];
+
+export const purpose = [
+  {id: 1, title: '개인 공부', icon: '📚'},
+  {id: 2, title: '노트북', icon: '👩‍💻'},
+  {id: 3, title: '비대면 회의', icon: '🖥️'},
+  {id: 4, title: '대면 회의', icon: '👥'},
+];
+
+export const workspace = [
+  {id: 1, title: '카페'},
+  {id: 2, title: '스터디룸'},
+  {id: 3, title: '스터디카페'},
+  {id: 4, title: '회의실'},
+  {id: 5, title: '도서관'},
+  {id: 6, title: '북카페'},
+  {id: 7, title: '라운지'},
+];
+
+export const termList = [
+  {
+    id: 0,
+    name: '1',
+    text: '서비스 이용 약관 동의',
+    value: 'check1',
+  },
+  {
+    id: 1,
+    name: '2',
+    text: '개인 정보 처리 방침',
+    value: 'check2',
+  },
+  {
+    id: 2,
+    name: '3',
+    text: '위치 정보 서비스 이용 약관',
+    value: 'check3',
+  },
+  {
+    id: 3,
+    name: '4',
+    text: '제 3자 정보 제공',
+    value: 'check4',
+  },
+  {
+    id: 4,
+    name: '5',
+    text: '만 14세 이상',
+    value: 'check5',
+  },
+  {
+    id: 5,
+    text: '제 3자 제공, 처리 위탁 동의 (선택)',
+    value: 'personal',
+  },
+  {
+    id: 6,
+    text: '마케팅 동의 (선택)',
+    value: 'marketing',
+  },
+];
