0fcea0a76290c247080bc45332c15939613d33fd
imssein <hyuntpdls@gmail.com>
2023-02-27 23:32:25 +0900

    feat: redux store 설정

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,8 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
 import LoginReducer from '../slice/LoginSlice';
+import ServiceTermReducer from '../slice/ServiceTermSlice';
 
 export const store = configureStore({
   reducer: {
     login: LoginReducer,
+    serviceterm: ServiceTermReducer,
   },
 });
