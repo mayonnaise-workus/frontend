5a03f233fe2d69a6fd6b48de2b7be23c86252e44
imssein <hyuntpdls@gmail.com>
2023-02-27 23:33:15 +0900

    fix: console.log() 삭제

--- src/redux/service/auth-header.ts
+++ src/redux/service/auth-header.ts
@@ -4,7 +4,6 @@ export const authHeader = async () => {
   try {
     const user = await AsyncStorage.getItem('user');
     const value = JSON.parse(user);
-    // console.log('user', value.access_token);
     return value.access_token;
   } catch (e) {
     console.log('error', e);
