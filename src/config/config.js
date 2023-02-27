582ee15725882fa49f3cc92cb2e0a3ee9580e0ef
imssein <hyuntpdls@gmail.com>
2023-02-28 00:34:54 +0900

    feat: config 설정

---
+++ src/config/config.js
@@ -0,0 +1,16 @@
+const BASE_URL =
+  'http://tune-env.eba-s7pcctn4.ap-northeast-2.elasticbeanstalk.com';
+
+export const MEMBER = {
+  AGREEMENT: `${BASE_URL}/member/agreement`,
+  MEMBER: `${BASE_URL}/member`,
+  ONBOARDING: `${BASE_URL}/member/onboarding`,
+  NAME: `${BASE_URL}/member/name`,
+  PURPOSE: `${BASE_URL}/member/preference/purpose`,
+  REGION: `${BASE_URL}/member/preference/region`,
+  WORKSPACE_PURPOSE: `${BASE_URL}/member/preference/workspace-purpose`,
+};
+
+export const LOGIN = {
+  KAKAO_LOGIN: `${BASE_URL}/login/kakao`,
+};
