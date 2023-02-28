2fa53918267e2dba0f1be55245e2af5be8cc6126
imssein <hyuntpdls@gmail.com>
2023-02-28 15:40:29 +0900

    fix: 이미지 경로 상수 처리

---
+++ assets/images.ts
@@ -0,0 +1,7 @@
+const images = {
+  LOGO_BLACK: require('./img/logo_black.png'),
+  WARNING_CIRCLE: require('./img/WarningCircle.png'),
+  LEFT_ICON: require('./img/CaretLeft.png'),
+};
+
+export default images;
