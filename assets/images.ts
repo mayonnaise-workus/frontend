167128b39e023b5caf07b756ceea3ff0cc27b2d9
imssein <hyuntpdls@gmail.com>
2023-02-28 16:26:20 +0900

    design: 이미지 추가

--- assets/images.ts
+++ assets/images.ts
@@ -2,6 +2,11 @@ const images = {
   LOGO_BLACK: require('./img/logo_black.png'),
   WARNING_CIRCLE: require('./img/WarningCircle.png'),
   LEFT_ICON: require('./img/CaretLeft.png'),
+  RIGHT_ICON: require('./img/rightIcon.png'),
+  BOOKMARK_ICON: require('./img/BookmarkSimple.png'),
+  PENCIL_ICON: require('./img/pencil.png'),
+  PROFILE_WHITE: require('./img/profileImg.png'),
+  PROFILE_GRAY: require('./img/ProfileImg2.png'),
 };
 
 export default images;
