3ffd3d270ce8dcf2b1bf2cb6bbf2955b0991ffc1
imssein <hyuntpdls@gmail.com>
2023-03-01 23:53:16 +0900

    design: 마이페이지 헤더 컴포넌트 구현

---
+++ src/components/mypage/Header/style.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.SafeAreaView`
+  flex-direction: row;
+  position: absolute;
+  top: 61.5px;
+`;
+
+export const Text = styled.Text`
+  font-weight: 600;
+  font-size: 18px;
+  position: absolute;
+  left: 145px;
+`;
+
+export const PressableBlock = styled.Pressable``;
+
+export const ImageBlock = styled.Image`
+  position: absolute;
+  left: 27.5px;
+  width: 10px;
+  height: 20px;
+`;
