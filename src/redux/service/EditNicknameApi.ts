e0430b3928a5ddcab5b67b4e6fa6dc7dc85880bf
imssein <hyuntpdls@gmail.com>
2023-03-02 01:25:35 +0900

    feat: 닉네임 수정 service 구현

---
+++ src/redux/service/EditNicknameApi.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import {Dispatch} from 'redux';
+import {
+  setUserLoading,
+  setUserError,
+  setUser,
+} from '../slice/EditNicknameSlice';
+import {MEMBER} from '../../config/config';
+import {authHeader} from './auth-header';
+import {Alert} from 'react-native';
+
+export const EditNicknameApi = (data: any) => {
+  return async (dispatch: Dispatch) => {
+    dispatch(setUserLoading(true));
+    const header = await authHeader();
+    try {
+      const response = await axios.post(MEMBER.NAME, data, {
+        headers: {Authorization: `Bearer ${header}`},
+      });
+      dispatch(setUser(response.data));
+      Alert.alert('저장 완료', '수정한 계정 정보를 저장했어요');
+      dispatch(setUserError(null));
+    } catch (error) {
+      dispatch(setUserError('An error occurred'));
+    }
+    dispatch(setUserLoading(false));
+  };
+};
