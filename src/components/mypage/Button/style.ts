4a9a994dfa3bd1b73aea3bbab0b2ab77d6283d68
imssein <hyuntpdls@gmail.com>
2023-03-02 00:00:11 +0900

    design: 마이페이지 버튼 컴포넌트 구현

---
+++ src/components/mypage/Button/style.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+import COLORS from '../../../../packages/colors';
+
+const Logo = styled.Image`
+  position: absolute;
+  width: 7.5px;
+  height: 15px;
+  position: absolute;
+  left: 319px;
+  top: 19.5px;
+`;
+
+const Text = styled.Text`
+  position: absolute;
+  margin-top: 8px;
+  font-weight: 500;
+  font-size: 17px;
+  color: ${COLORS.GRAY_1};
+  left: 16px;
+`;
+
+const Wrapper = styled.View`
+  flex-direction: row;
+  width: 350px;
+  height: 54px;
+  margin-bottom: 12px;
+  border-radius: 10px;
+  background-color: #f4f5f7;
+  align-items: center;
+`;
+
+export {Logo, Wrapper, Text};
