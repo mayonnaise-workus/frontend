5b6e5156e70528a336b35239df79220ae07d53d4
imssein <hyuntpdls@gmail.com>
2023-03-03 01:03:20 +0900

    refactor: user이 있을 때만 return 하도록 설정

--- src/redux/service/auth-header.ts
+++ src/redux/service/auth-header.ts
@@ -3,8 +3,9 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 export const authHeader = async () => {
   try {
     const user = await AsyncStorage.getItem('user');
-    const value = JSON.parse(user);
-    return value.access_token;
-  } catch (e) {
-  }
+    if (user) {
+      const value = JSON.parse(user);
+      return value.access_token;
+    }
+  } catch (e) {}
 };
