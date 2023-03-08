0af28e8f8dbafb88af069547bd23b0c2806b587e
imssein <hyuntpdls@gmail.com>
2023-03-08 17:02:08 +0900

    feat: 워크 스페이스 리스트 기능 구현

--- src/redux/service/WorkSpaceListApi.ts
+++ src/redux/service/WorkSpaceListApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {Dispatch} from 'redux';
 import {setLoading, setError, setData} from '../slice/WorkSpaceListSlice';
-import {WORKSPACE} from '../../config/config';
+import {MEMBER} from '../../config/config';
 import {authHeader} from './auth-header';
 
 export const WorkSpaceListApi = () => {
@@ -9,12 +9,9 @@ export const WorkSpaceListApi = () => {
     dispatch(setLoading(true));
     const header = await authHeader();
     try {
-      const response = await axios.get(
-        `${WORKSPACE.WORKSPACE_LIST}?region=1,2`,
-        {
-          headers: {Authorization: `Bearer ${header}`},
-        },
-      );
+      const response = await axios.get(`${MEMBER.FAVORITE_WORKSPACE}`, {
+        headers: {Authorization: `Bearer ${header}`},
+      });
       dispatch(setData(response.data));
       dispatch(setError(null));
     } catch (error) {
