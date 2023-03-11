f967d50465309b93cd6b0b5a447535c0b72d2720
GodDorae <dohyun6425@naver.com>
2023-03-11 11:21:41 +0900

    style: 회원 탈퇴 완료 화면 수정

--- src/screens/MemberCancellationCompleteScreen/MemberCancellationCompleteScreen.tsx
+++ src/screens/MemberCancellationCompleteScreen/MemberCancellationCompleteScreen.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import {RouteProp} from '@react-navigation/native';
-import {SafeAreaView} from 'react-native';
 import images from '../../../assets/images';
-import {Image, Message, Pressable, Text, X} from './style';
+import {
+  Container,
+  ContentContainer,
+  Image,
+  Message,
+  Pressable,
+  Text,
+  X,
+} from './style';
 import {
   MyScreenStackNavigationProps,
   MyScreenStackParamList,
@@ -19,19 +26,21 @@ function MemberCancellationCompleteScreen({navigation}: IProps) {
   }
 
   return (
-    <SafeAreaView>
+    <Container>
       <Pressable onPress={handlePress}>
         <X source={images.X} />
       </Pressable>
-      <Image source={images.LOGO} />
-      <Text>회원 탈퇴가 완료 되었습니다.</Text>
-      <Message>
-        {`회원으로 함께해 주셔서 감사합니다! 
+      <ContentContainer>
+        <Image source={images.LOGO} />
+        <Text>회원 탈퇴가 완료 되었습니다.</Text>
+        <Message>
+          {`회원으로 함께해 주셔서 감사합니다! 
 서비스 이용이 도움이 되셨기를 바랍니다. 
 앞으로도 많은 응원 부탁드리고
 필요하실 때 돌아오실 수 있도록 더 노력하겠습니다.`}
-      </Message>
-    </SafeAreaView>
+        </Message>
+      </ContentContainer>
+    </Container>
   );
 }
 
