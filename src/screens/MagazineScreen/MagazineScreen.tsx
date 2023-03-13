ba940f21346d976e531cdc5408f8a3010901523a
imssein <hyuntpdls@gmail.com>
2023-03-13 20:55:37 +0900

    feat: 매거진 메인 페이지 구현

--- src/screens/MagazineScreen/MagazineScreen.tsx
+++ src/screens/MagazineScreen/MagazineScreen.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {RootState} from '../../redux/store/store';
+import {Magazine} from '../../redux/service/Magazine';
+import styled from 'styled-components/native';
+import {ScrollView} from 'react-native-gesture-handler';
+import SubHeader from '../../components/common/SubHeader';
 
 const MagazineScreen = () => {
+  const {data} = useSelector((state: RootState) => state.magazine);
+  const [magazine, setMagazine] = useState<string[]>([]);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    Magazine()(dispatch);
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (data) {
+      setMagazine(data.list);
+    }
+  }, [data]);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Magazine!</Text>
-    </View>
+    <Container>
+      <SubHeader />
+      {magazine &&
+        magazine.map(item => (
+          <ScrollView>
+            <ImageBlock>
+              <Image source={{uri: `${item.title_image}`}} />
+            </ImageBlock>
+          </ScrollView>
+        ))}
+    </Container>
   );
 };
 
 export default MagazineScreen;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 50,
-    fontWeight: 'bold',
-  },
-});
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  background-color: white;
+`;
+export const ImageBlock = styled.Pressable`
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Image = styled.Image`
+  height: 350px;
+  width: 350px;
+  border-radius: 10px;
+  margin-bottom: 12px;
+`;
