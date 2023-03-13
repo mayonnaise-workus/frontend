7e65aeedd2a048216c754062cb91c0ee0e0c5984
imssein <hyuntpdls@gmail.com>
2023-03-13 20:55:46 +0900

    feat: 서브 헤더 구현

---
+++ src/components/common/SubHeader.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import images from '../../../assets/images';
+
+const LogoContainer = styled.View`
+  flex-direction: row;
+  height: 40px;
+  justify-content: center;
+  align-items: center;
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-top: 11px;
+  margin-bottom: 11px;
+`;
+
+const EmptyView = styled.View`
+  width: 20px;
+  height: 20px;
+`;
+
+const Logo = styled.Image`
+  width: 20px;
+  height: 20px;
+`;
+
+function SubHeader() {
+  return (
+    <LogoContainer>
+      <EmptyView />
+      <Logo source={images.HEADER_LOGO} />
+    </LogoContainer>
+  );
+}
+
+export default SubHeader;
