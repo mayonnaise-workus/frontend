726a17d9d30e193ff1409afa197e986e9ee895e5
imssein <hyuntpdls@gmail.com>
2023-03-23 20:14:52 +0900

    fix: 회원가입 로직 변경에 따른 파일 삭제

--- src/redux/slice/SignUpDataSlice.ts
+++ src/redux/slice/SignUpDataSlice.ts
@@ -3,51 +3,27 @@ import {createSlice} from '@reduxjs/toolkit';
 interface ApiState {
   loading: boolean;
   error: string | null;
-  access_token: string | null;
-  refresh_token: string | null;
-  expires_in: number | undefined;
+  data: number | null;
   name: string;
   marketing_agreement: boolean;
   personal_information_agreement: boolean;
   purpose_ids: Array<string>;
   location_ids: Array<string>;
   workspace_purpose_ids: Array<string>;
-  kakao: boolean;
-  kakaoValue: string | null;
-  google: boolean;
-  googleValue: string | null;
-  apple: boolean;
-  appleValue: string | null;
-  id: string | null;
-  serverAuthCode: string | null;
-  authorizationCode: string | null;
-  user: string | null;
 }
 const initialState: ApiState = {
   loading: false,
   error: null,
-  access_token: '',
-  refresh_token: '',
-  expires_in: undefined,
+  data: null,
   name: '',
   marketing_agreement: false,
   personal_information_agreement: false,
   purpose_ids: [],
   location_ids: [],
   workspace_purpose_ids: [],
-  kakao: false,
-  kakaoValue: '',
-  google: false,
-  googleValue: '',
-  apple: false,
-  appleValue: '',
-  id: '',
-  serverAuthCode: '',
-  authorizationCode: '',
-  user: '',
 };
 
-const signUpSlice = createSlice({
+const SignUpDataSlice = createSlice({
   name: 'signUp',
   initialState,
   reducers: {
@@ -57,44 +33,8 @@ const signUpSlice = createSlice({
     setError(state, action) {
       state.error = action.payload;
     },
-    setKakao(state, action) {
-      state.kakao = action.payload;
-    },
-    setKakaoValue(state, action) {
-      state.kakaoValue = action.payload;
-    },
-    setAccess_token(state, action) {
-      state.access_token = action.payload;
-    },
-    setRefresh_token(state, action) {
-      state.refresh_token = action.payload;
-    },
-    setExpires_in(state, action) {
-      state.expires_in = action.payload;
-    },
-    setGoogle(state, action) {
-      state.google = action.payload;
-    },
-    setGoogleValue(state, action) {
-      state.googleValue = action.payload;
-    },
-    setId(state, action) {
-      state.id = action.payload;
-    },
-    setServerAuthCode(state, action) {
-      state.serverAuthCode = action.payload;
-    },
-    setApple(state, action) {
-      state.apple = action.payload;
-    },
-    setAppleValue(state, action) {
-      state.appleValue = action.payload;
-    },
-    setAuthorizationCode(state, action) {
-      state.authorizationCode = action.payload;
-    },
-    setUser(state, action) {
-      state.user = action.payload;
+    setData(state, action) {
+      state.data = action.payload;
     },
     setName(state, action) {
       state.name = action.payload;
@@ -120,24 +60,12 @@ const signUpSlice = createSlice({
 export const {
   setLoading,
   setError,
-  setKakao,
-  setKakaoValue,
-  setAccess_token,
-  setRefresh_token,
-  setExpires_in,
-  setGoogle,
-  setGoogleValue,
-  setId,
-  setServerAuthCode,
-  setApple,
-  setAppleValue,
-  setAuthorizationCode,
-  setUser,
+  setData,
   setName,
   setMarketing_agreement,
   setPersonal_information_agreement,
   setPurpose_ids,
   setLocation_ids,
   setWorkspace_purpose_ids,
-} = signUpSlice.actions;
-export default signUpSlice.reducer;
+} = SignUpDataSlice.actions;
+export default SignUpDataSlice.reducer;
